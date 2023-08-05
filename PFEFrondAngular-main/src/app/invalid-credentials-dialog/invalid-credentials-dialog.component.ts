8c4290fb79230404ae5ed042aeaaaabc0ee324a7
jihen303 <129227172+jihen303@users.noreply.github.com>
2023-08-05 15:26:41 +0100

    new

--- PFEFrondAngular-main/src/app/invalid-credentials-dialog/invalid-credentials-dialog.component.ts
+++ PFEFrondAngular-main/src/app/invalid-credentials-dialog/invalid-credentials-dialog.component.ts
@@ -4,7 +4,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-invalid-credentials-dialog',
   templateUrl: './invalid-credentials-dialog.component.html',
-  styleUrls: ['./invalid-credentials-dialog.component.css'] // Vérifiez que le chemin est correct ici
+  styleUrls: ['./invalid-credentials-dialog.component.scss'] // Vérifiez que le chemin est correct ici
 })
 export class InvalidCredentialsDialogComponent {
   constructor(private dialogRef: MatDialogRef<InvalidCredentialsDialogComponent>) {}
